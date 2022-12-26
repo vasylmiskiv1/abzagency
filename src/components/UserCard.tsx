3ac13795a203936f4a57fcaaff3a94582bc38b0f
Vasyl <vasylmiskiv@gmail.com>
2022-12-26 18:10:32 +0200

    fix display card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,7 +2,7 @@ import Tooltip from "./Tooltip";
 
 const UserCard = ({ user }: UserProps) => {
   return (
-    <div className="p-5 bg-white flex flex-col items-center rounded-xl xl:w-[418px] lg:w-[296px] md:w-[345px] max-sm: w-[355px]">
+    <div className="p-5 bg-white flex flex-col items-center rounded-xl xl:w-[415px] lg:w-[296px] md:w-[345px] max-sm: w-[355px]">
       <img className="rounded-full mb-5" src={user.photo} alt="user-img" />
       <Tooltip text={user.name} />
       <p className="pt-5 pb-1 break-all">{user.position}</p>
